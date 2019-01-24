d473b6ea0f14ae48e4ba7032c681185f06184e09
Dan Sherman <dvsherman@protonmail.com>
2019-01-24 14:22:11 -0700

    created class Person

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,17 @@
 // CODE here for your Lambda Classes
+
+// Person
+
+class Person {
+  constructor (pAttrs) {
+    this.name = pAttrs.name;
+    this.age = pAttrs.age;
+    this.location = pAttrs.location;
+  }
+}
+
+// Instructor
+
+// Student
+
+// Project Manager
